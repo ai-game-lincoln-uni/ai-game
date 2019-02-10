2d87281627ae5d9e68a96731f8d06d4cc56b1e43
AlexanderJStaniforth <47030515+AlexanderJStaniforth@users.noreply.github.com>
2019-02-10 19:59:00 +0000

    Fit label encoder and return encoded labels
    Fit label encoder and return encoded labels

--- Game/neuralnet.py
+++ Game/neuralnet.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from pandas import read_csv
 from keras.models import Sequential
@@ -11,11 +12,9 @@ dataset = read_csv("connect4.csv", delimiter=",") #reads in file. http://archive
 
 X = dataset.iloc[:,0:42].values 
 Y = dataset.iloc[:,42:43].values
-#encoder = LabelEncoder()
-#encoder.fit(X)
-#encoded_X = encoder.transform(X)
-#encoder.fit(Y)
-#encoded_Y = encoder.transform(Y)
+
+le = preprocessing.LabelEncoder()
+X = le.fit_transform 
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
@@ -27,7 +26,7 @@ model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
 
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='rmsprop', loss='binary_crossentropy',metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=20,batch_size=128)
 
