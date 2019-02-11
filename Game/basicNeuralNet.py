c3f32cb66d5a45049ed65101191bb1b757ff9beb
LordOfPolls <ddavidallen13@gmail.com>
2019-02-11 11:33:53 +0000

    Converted basicNeuralNet to use logging

--- Game/basicNeuralNet.py
+++ Game/basicNeuralNet.py
@@ -1,91 +1,94 @@
-print ('Program Begin\n')
+import logging
+from logManager import log
+
+log.info('Program Begin\n')
 
 
-print('Importing Packages')
+log.info('Importing Packages')
 import tensorflow as tf
-print('\tImported TensorFlow')			#TensorFlow 1.5
+log.debug('\tImported TensorFlow')			#TensorFlow 1.5
 import keras
-print('\tImported Keras')
+log.debug('\tImported Keras')
 import numpy as np
-print('\tImported Numpy')
+log.debug('\tImported Numpy')
 #import matplotlib.pyplot as plt			#Just to see end result, not required for purpose
 #print('\tImported Matplotlib.Pyplot')
-print('\tImporting Done\n')
+log.info('\tImporting Done\n')
 
 
-print('Setting Initial Variables')
+log.debug('Setting Initial Variables')
 bannerChar = '¬'
-print('\tVariables Set\n')
+log.debug('\tVariables Set\n')
 
 
 
-print('Creating Dataset')
+log.debug('Creating Dataset')
 mnist = keras.datasets.mnist			#In practice, would probably be replaced with X000 possible game states
-print('\tDataset Created\n')
+log.debug('\tDataset Created\n')
 
 
-print('Loading Data')
+log.debug('Loading Data')
 (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
-print('\tData Loaded\n')
+log.debug('\tData Loaded\n')
 
 
-print('Normalising Data')
+log.debug('Normalising Data')
 xTrain = keras.utils.normalize(xTrain, axis=1)			#In this example, reduces pix values [0-255] to [0-1]
 xTest = keras.utils.normalize(xTest, axis=1)			#Probably not necessary if board values [0-2]
-print('\tData Normalised\n')
+log.debug('\tData Normalised\n')
 
 
-print('Creating Model')
+log.info('Creating Model')
 model = keras.models.Sequential()
-print('\tModel Created\n')
+log.info('\tModel Created\n')
 
 
-print('Adding to Model')
+log.debug('Adding to Model')
 model.add(keras.layers.Flatten())
-print('\tInput Nodes Added')
+log.debug('\tInput Nodes Added')
 model.add(keras.layers.Dense(128, activation=tf.nn.relu))
-print('\tNode Layer Added')
+log.debug('\tNode Layer Added')
 model.add(keras.layers.Dense(128, activation=tf.nn.relu))
-print('\tNode Layer Added')
+log.debug('\tNode Layer Added')
 model.add(keras.layers.Dense(10, activation=tf.nn.softmax))			#10 to be replaced with 7, for possible moves
-print('\tOutput Layer Added\n')
+log.debug('\tOutput Layer Added\n')
 
 
-print('Compiling Model')
+log.info('Compiling Model')
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',				#Still need to look into this
               matrics=['accuracy'])
-print('\tModel Compiled\n')
+log.info('\tModel Compiled\n')
 
 
-print('Fitting Model')
+log.info('Fitting Model')
 model.fit(xTrain, yTrain, epochs=3)
-print('\tModel Fitted\n')
+log.info('\tModel Fitted\n')
 
 
-print('Evaluating Model')
+log.info('Evaluating Model')
 print(model.evaluate(xTest, yTest))
-print('\tModel Evaluated\n')
+log.info('\tModel Evaluated\n')
 
 
-print('Saving Model')
+log.info('Saving Model')
 model.save('first.model')						#Would be the model actually used to play the game, once trained
-print('\tModel Saved\n')
+log.info('\tModel Saved\n')
 
 
-print('Loading Model')
+log.info('Loading Model')
 newModel = keras.models.load_model('first.model')
-print('\tModel Loaded\n')
+log.info('\tModel Loaded\n')
 
 
-print('Predicting')
+log.info('Predicting')
 predictions = newModel.predict([xTest])		#Where xTest might be a board, still needs looking into
 print('\tPrediction of val 19: ', np.argmax(predictions[19]))
 #plt.imshow(xTest[19])						#Used for testing to see accuracy
 #plt.show()
-print('\tPredicted\n')
+log.info('\tPredicted\n')
 
 
 
-print('Program End')
+log.info('Program End')
 for _ in range (0, 50): print(bannerChar, end='')
