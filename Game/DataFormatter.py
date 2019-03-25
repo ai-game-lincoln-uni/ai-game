302ba97bdd36dfb68bd548efd4688e9f717df542
GMBroadbent <34782917+gmbroadbent@users.noreply.github.com>
2019-03-25 19:18:51 +0000

    Update DataFormatter.py
    Removed redundant Imports
    Standardisation

--- Game/DataFormatter.py
+++ Game/DataFormatter.py
@@ -1,15 +1,8 @@
 import logging
 from logManager import log
 
-log.info('Program Begin\n')
-
-
-log.info('Importing Packages')
-import os
-log.debug('\tImported OS')
-import numpy as np
-log.debug('\tImported Numpy')
-log.info('\tImporting Done\n')
+log.info('DataFormatter')
+log.info('\tProgram Begin\n')
 
 
 def _format_array(file):    # A big clusterfuck of I don't know what
@@ -49,26 +42,24 @@ def _format_array(file):    # A big clusterfuck of I don't know what
 
 def _format_array_v2(file):    # This one actually works :)
     output = []
-    rawData = open(file, 'r').read()    # reads the data from specified file as string
+    raw_data = open(file, 'r').read()    # reads the data from specified file as string
 
-    rawData = rawData.replace("[", "")
-    rawData = rawData.replace("]", "")    # reduces string to essential characters
-    rawData = rawData.replace(",", "")
+    raw_data = raw_data.replace("[", "")
+    raw_data = raw_data.replace("]", "")    # reduces string to essential characters
+    raw_data = raw_data.replace(",", "")
 
-    length = len(rawData)
+    length = len(raw_data)
 
-    char = rawData[0]
-
-    strNum = ""    # Buffer to hold characters for single value
+    str_num = ""    # Buffer to hold characters for single value
 
     for i in range(length):
-        char = rawData[i]
+        char = raw_data[i]
         if not char == " ":    # If char is .|0|1|2
-            strNum += char
+            str_num += char
 
         if char == " " or i == length:    # If reached end of number
-            Num = float(strNum)
-            output.append(Num)
-            strNum = ""    # Resets buffer
+            num = float(str_num)
+            output.append(num)
+            str_num = ""    # Resets buffer
 
     return output
